25f58ff7733f88cb7d794d37d6ec10e5fa751616
Hulian2000 <prepmoringa11@gmail.com>
2019-09-08 15:55:42 +0300

    This  commit adds and hides the api key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     NEWS_HIGHLIGHT_API_BASE_URL ='https://newsapi.org/v2/sources?&apiKey={}'
-    NEWS_HIGHLIGHT_API_KEY = os.environ.get('NEWS_HIGHLIGHT_API_KEY')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     TOP_HEADLINES_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     EVERYTHING_URL_KEY ='https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&apiKey={}'
 
