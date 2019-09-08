56188fe0caf660dbc4bf1faf1622ba8b43cc5fb9
Hulian2000 <prepmoringa11@gmail.com>
2019-09-08 20:46:16 +0300

    This commit displays the title in the of our page

---
+++ app/main/views.py
@@ -0,0 +1,17 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_Sources(), get_everything, get_top_headlines
+from ..models import Sources, Top_Headlines, Everything 
+
+# views
+@main.route('/')
+def index():
+    '''
+    view root function that returns the index page and its data
+    '''
+    Sources = get_Sources
+    Everything = get_everything
+    print(Sources)
+
+    title = 'News Top highlights'
+    return render_template(title= title,Sources=Sources,Everything=Everything)
