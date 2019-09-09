d7aef495e2e95eb966b9b088c8f3fc3b20b74a57
Hulian2000 <prepmoringa11@gmail.com>
2019-09-09 11:56:35 +0300

    This commit registers  main blueprint in the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     # Registering the blueprint
-    
+    from .main import main as main_blueprint
+    app.register_blueprint(main-blueprint)
 
     # setting configurations
-
+    from .requests import configure_request
+    configure_request(app)
 
     return app
 
