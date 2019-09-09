0a83230b28cbe18285ccbd5ea49da95e065462b2
Hulian2000 <prepmoringa11@gmail.com>
2019-09-09 11:25:41 +0300

    create the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,16 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
+    # create app configurations
+    app
+
+    # initialize flask extensions
+
+    # Registering the blueprint
+
+    # setting configurations
+
+
+    return app
+
     
